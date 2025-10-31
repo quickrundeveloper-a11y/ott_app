be2da28256acdb07f0145550874de9cc7ae00a95
Raj Tiwari <skoobfy@gmail.com>
2025-10-31 18:02:08 +0530

    Initial commit

---
+++ lib/sample_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SampleFile extends StatefulWidget {
+  const SampleFile({super.key});
+
+  @override
+  State<SampleFile> createState() => _SampleFileState();
+}
+
+class _SampleFileState extends State<SampleFile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
