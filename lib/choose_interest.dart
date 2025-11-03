ab5bba0bc680ba5f1f4253515fd29800a5b49e48
Raj Tiwari <skoobfy@gmail.com>
2025-11-03 12:11:43 +0530

    interst page

---
+++ lib/choose_interest.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseInterest extends StatefulWidget {
+  const ChooseInterest({super.key});
+
+  @override
+  State<ChooseInterest> createState() => _ChooseInterestState();
+}
+
+class _ChooseInterestState extends State<ChooseInterest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
